224ab4cd69a2bf2a3e23de358334b6e72027264b
Akhand Pratap Singh <akhndprtp@gmail.com>
2026-07-18 23:54:08 +0530

    feat: Developed functions' arguments declarations understanding

---
+++ 02-Basics/functions.js
@@ -0,0 +1,12 @@
+function addTwo(num) {
+    return num + 2;
+}
+addTwo(5);
+function getUpper(val) {
+    return val.toUpperCase();
+}
+function signUpUser(name, email, isPaid = false) { }
+;
+signUpUser('Akhand', 'akhndprtp@gmail.com', false);
+signUpUser('Akhand', 'akhndprtp@gmail.com');
+export {};
---
+++ 02-Basics/functions.ts
@@ -0,0 +1,17 @@
+function addTwo(num:number){
+    return num + 2;
+}
+
+addTwo(5);
+
+function getUpper(val:string){
+    return val.toUpperCase();
+}
+
+function signUpUser(name:string, email:string, isPaid:boolean=false){};
+
+signUpUser('Akhand', 'akhndprtp@gmail.com', false);
+
+signUpUser('Akhand', 'akhndprtp@gmail.com');
+
+export {}
